683a0a88152f7e0a9779b4db28bb5587e3a3e0c9
Remi <dragon.remi.44@livr.fr>
2020-01-27 15:49:54 +0100

    mise en place de l'index

---
+++ index.js
@@ -0,0 +1,9 @@
+var texte;
+function Send() {
+  document.querySelector("#choix").value;
+  if (document.querySelector("#choix").value == "1") {
+    console.log("Bonjour");
+    texte = "Bonjour";
+    this.texte = "Bonjour";
+  }
+}
