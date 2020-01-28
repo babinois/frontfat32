e27aa50171cdd704c89dd80d4758847007364945
leo Rigaudeau <leo.rigaudeau@ynov.com>
2020-01-28 22:50:09 +0100

    stylisation + init des page présente dans le blockchaine

--- index.js
+++ index.js
@@ -1,47 +1,75 @@
-let i = 0;
-var block = httpGet("http://localhost:5001/blockchain");
-while (i < block.fivepages.data.length) {
-  let active = "";
-  let classActive = "";
-  if (i == 0) {
-    active = "active";
-    classActive = 'class="active"';
-  }
-  document.getElementById("display").innerHTML += `
-<div class="carousel-item ${active}">
-      <div class="view">
-        <img class="d-block img" src="http://iwallpapers2.free.fr/images/Photographie/Black_Collection/Fond_texture_Noir_1920x1080.jpg"
-          alt='${i}'>
-        <div class="mask rgba-black-light"></div>
-      </div>
-      <div class="carousel-caption2">
-        <p id="text${i}"></p>
-      </div>
-    </div>`;
-  document.getElementById(
-    "carouselIndicator"
-  ).innerHTML += `<li data-target="#carousel-example-2" data-slide-to="${i}" ${classActive}></li>`;
-  displayText(i);
-  i++;
-}
+let i = 1;
+constructPage();
 
-function displayText(id) {
-  let j = 0;
-  while (j < block.fivepages.data[id].content.length) {
-    document.getElementById("text" + id).innerHTML +=
-      block.fivepages.data[id].content[j];
-    j++;
-  }
+function displayText(id, blockchain) {
+  var inner = "Page non trouvé dans la BlockChain <br> BiP bip Bip bipppp..";
+  blockchain.forEach(block => {
+    block.fivePages.data.forEach(page => {
+      if (page.idPage == id) {
+        inner = concat(page.content);
+      }
+    });
+  });
+  document.getElementById("text" + id).innerHTML += inner;
 }
 
 function changePage() {
-  console.log(document.getElementById("newPage").value);
-  console.log(httpGet("http://localhost:5001/blockchain"));
+  resetPage();
+  i = document.getElementById("newPage").value;
+  constructPage();
+}
+
+function concat(arrayStr) {
+  var strconcat = "";
+  arrayStr.forEach(str => {
+    strconcat += " " + str + " <br>";
+  });
+  return strconcat;
+}
+
+function resetPage() {
+  while (document.getElementById("display").childNodes.length > 0) {
+    document.getElementById("display").childNodes[0].remove();
+  }
+  while (document.getElementById("carouselIndicator").childNodes.length > 0) {
+    document.getElementById("carouselIndicator").childNodes[0].remove();
+  }
 }
 
 function httpGet(theUrl) {
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.open("GET", theUrl, false); // false for synchronous request
   xmlHttp.send(null);
-  return xmlHttp.responseText;
+  return JSON.parse(xmlHttp.responseText);
+}
+
+function constructPage() {
+  i -= 1;
+  var blockchain = httpGet("http://localhost:5001/blockchain");
+  if (blockchain) {
+    for (let j = 0; j < 4; j++) {
+      let active = "";
+      let classActive = "";
+      if (j === 0) {
+        active = "active";
+        classActive = 'class="active"';
+      }
+      document.getElementById("display").innerHTML += `
+      <div class="carousel-item ${active}">
+        <div class="view">
+          <img class="d-block img" src="http://iwallpapers2.free.fr/images/Photographie/Black_Collection/Fond_texture_Noir_1920x1080.jpg"
+            alt='${i}'>
+          <div class="mask rgba-black-light"></div>
+        </div>
+        <div class="carousel-caption2">
+          <p id="text${i}"></p>
+        </div>
+      </div>`;
+      document.getElementById(
+        "carouselIndicator"
+      ).innerHTML += `<li data-target="#carousel-example-2" data-slide-to="${i}" ${classActive}></li>`;
+      displayText(i, blockchain);
+      i++;
+    }
+  }
 }
