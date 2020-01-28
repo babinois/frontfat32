bdfd1e478c03df839e9a67e8c2e0470815481f5f
Remi <dragon.remi.44@livr.fr>
2020-01-28 18:13:05 +0100

    mise en place de la requete blokchain

--- index.js
+++ index.js
@@ -1,67 +1,5 @@
 let i = 0;
-let block = {
-  fivepages: {
-    data: [
-      {
-        content: [
-          "Guy de Maupassant ",
-          "Bel-Ami ",
-          "roman ",
-          "La Bibliothèque électronique du Québec ",
-          "Collection ",
-          "À tous les vents ",
-          "Volume 510 %3A version 1.01",
-          "2"
-        ],
-        idPage: "1"
-      },
-      {
-        content: [
-          "Du même auteur%2C à la Bibliothèque ",
-          "Mademoiselle Fifi ",
-          "Contes de la bécasse ",
-          "Pierre et Jean ",
-          "Sur l’eau ",
-          "La mais",
-          "on Tellier ",
-          "La petite Roque ",
-          "Une vie ",
-          "Fort comme la mort ",
-          "Clair de lune ",
-          "Miss Harriet ",
-          "La main gauche ",
-          "Yvette ",
-          "L’inutile beauté ",
-          "Monsieur Parent ",
-          "Le Horla ",
-          "Les soeurs Rondoli",
-          "Le docteur Héraclius Gloss et autres contes ",
-          "Les dimanches d’un bourgeois de Paris ",
-          "Le rosier de Madame Husson ",
-          "Contes du jour et de la nuit ",
-          "La vie errante ",
-          "Notre coeur ",
-          "3"
-        ],
-        idPage: 2
-      },
-      {
-        content: [
-          "Bel-Ami ",
-          "Édition de référence %3A ",
-          "Éditions Rencontre%2C Lausanne. ",
-          "Texte établi et présenté par Gilbert Sigaux. ",
-          "4"
-        ],
-        idPage: 3
-      },
-      {
-        content: ["Première partie ", "5"],
-        idPage: 4
-      }
-    ]
-  }
-};
+var block = httpGet("http://localhost:5001/blockchain");
 while (i < block.fivepages.data.length) {
   let active = "";
   let classActive = "";
@@ -90,11 +28,20 @@ while (i < block.fivepages.data.length) {
 function displayText(id) {
   let j = 0;
   while (j < block.fivepages.data[id].content.length) {
-    document.getElementById("text" + id).innerHTML += block.fivepages.data[id].content[j];
+    document.getElementById("text" + id).innerHTML +=
+      block.fivepages.data[id].content[j];
     j++;
   }
 }
 
 function changePage() {
   console.log(document.getElementById("newPage").value);
+  console.log(httpGet("http://localhost:5001/blockchain"));
+}
+
+function httpGet(theUrl) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", theUrl, false); // false for synchronous request
+  xmlHttp.send(null);
+  return xmlHttp.responseText;
 }
